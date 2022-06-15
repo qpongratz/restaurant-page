0abbee64c803e3a2f0258453a31f77699071b882
Quentin Pongratz <qpongratz@gmail.com>
2022-06-15 14:13:22 -0500

    Change text

--- src/homeTab.js
+++ src/homeTab.js
@@ -31,14 +31,14 @@ export default function homeTab() {
   content.appendChild(domElement('button', 'Press me', 'nacho-button'));
 
   content.appendChild(domElement('div', '', 'banner'))
-  content.appendChild(domElement('h1', 'Fully Loaded Nacho Emporium', 'restaurant_name'));
+  content.appendChild(domElement('h1', 'Midwest Marty\'s Fully Loaded Nacho Emporium', 'restaurant_name'));
   content.appendChild(domElement('h2', 'Welcome!'))
   content.appendChild(domElement('p', 'Come on down to the Fully Loaded Nacho Emporium. The only family-friendly, fully-loaded, nacho establishment in the greater metropolitan area that doesn\'t require you to be Fully Loaded'));
   content.appendChild(domElement('h2', 'Rules:'));
 
   const rules = [
     'Have Fun',
-    'If you get nachos to share...'
+    'If you order nachos to share, one person can\'t just eat all of the fully loaded nachos. Basically, if two people order nachos to share, one person can\'t take the ones with meat and stuff.'
   ];
 
   content.appendChild(createList(false, rules));
