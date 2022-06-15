2bf2f614534558f1d7e89062bf9189569829b9a2
Quentin Pongratz <qpongratz@gmail.com>
2022-06-15 09:17:11 -0500

    Make domElement function

--- src/homeTab.js
+++ src/homeTab.js
@@ -1,6 +1,16 @@
 export default function homeTab() {
   const content = document.getElementById('content');
-
-  content.innerText = 'Hello, World!';
   content.classList.add('card');
+
+  // Move this to its own thing later. Needs to be reused in other tabs
+  const domElement = (tag, text, cssClass = false) => {
+    const element = document.createElement(tag);
+    element.innerText = text;
+    if (cssClass) { element.classList.add(cssClass) }
+    return element;
+  }
+
+
+  content.appendChild(domElement('p', 'Added by function'))
+
 }
