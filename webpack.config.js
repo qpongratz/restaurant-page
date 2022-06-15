044e8a7debb125cd25a3a04bc87d2dcded079d18
Quentin Pongratz <qpongratz@gmail.com>
2022-06-15 15:10:04 -0500

    Add font

--- webpack.config.js
+++ webpack.config.js
@@ -17,6 +17,10 @@ module.exports = {
         test: /\.(png|svg|jpg|jpeg|gif)$/i,
         type: 'asset/resource',
       },
+      {
+        test: /\.ttf$/i,
+        type: 'asset/resource',
+      },
     ],
   },
 };
