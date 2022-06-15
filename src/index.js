8ec2d4bf3ef2c1a3525343ec29e7efe6f1f7c6f8
Quentin Pongratz <qpongratz@gmail.com>
2022-06-15 15:54:52 -0500

    Hook up button logic

--- src/index.js
+++ src/index.js
@@ -6,8 +6,39 @@ import navBar from './navBar';
 const content = document.getElementById('content');
 content.classList.add('card');
 
+class Tabs {
+  home(){
+    this.clear();
+    homeTab(content);
+  }
+  menu(){
+    this.clear();
+    menuTab(content);
+  }
+  about(){
+    this.clear();
+    aboutTab(content);
+  }
+  contact(){
+    this.clear();
+    contactTab(content);
+  }
+  clear(){
+    while (content.firstChild) {
+      content.firstChild.remove();
+    };
+  }
+}
+
 navBar();
+homeTab(content);
 
 const navigation = document.querySelectorAll('nav>button');
-console.log(navigation);
-menuTab(content);
+
+let tab = new Tabs();
+
+navigation.forEach (button => {
+  button.addEventListener('click', () => {
+    tab[button.id]();
+  });
+});
