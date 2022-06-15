c1e97ad83b994ff4a6f1781ab94046766b9c864a
Quentin Pongratz <qpongratz@gmail.com>
2022-06-15 10:46:59 -0500

    Change function names

--- src/homeTab.js
+++ src/homeTab.js
@@ -6,14 +6,14 @@ export default function homeTab() {
   content.classList.add('card');
 
   // Move this to its own thing later. Needs to be reused in other tabs
-  const textElement = (tag, text, cssClass = false) => {
+  const domElement = (tag, text, cssClass = false) => {
     const element = document.createElement(tag);
     element.innerText = text;
     if (cssClass) { element.classList.add(cssClass) }
     return element;
   }
-  const addTextToContent = (tag, text, cssClass = false) => {
-    content.appendChild(textElement(tag, text, cssClass));
+  const addElementToContent = (tag, text, cssClass = false) => {
+    content.appendChild(domElement(tag, text, cssClass));
   }
 
   const addImageToContent = (name, cssClass = false) => {
@@ -22,12 +22,10 @@ export default function homeTab() {
     if (cssClass) { image.classList.add(cssClass) }
     content.appendChild(image);
   }
-  addTextToContent('button', 'Press me', 'nacho-button');
+  addElementToContent('button', 'Press me', 'nacho-button');
 
   addTextToContent('h1', 'Fully Loaded Nacho Emporium', 'restaurant_name')
   addTextToContent('p', 'Come on down to the Fully Loaded Nacho Emporium. The only family-friendly, fully-loaded, nacho establishment in the greater metropolitan area that doesn\'t require you to be Fully Loaded')
   addTextToContent('p', 'Added by function');
   addImageToContent(Enjoyment);
-  addImageToContent(Nacho);
-
 }
