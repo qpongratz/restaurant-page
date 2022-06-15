a073135fd9cec49da8e88a5707e94505bd1eaea0
Quentin Pongratz <qpongratz@gmail.com>
2022-06-15 09:32:58 -0500

    Add image function

--- src/homeTab.js
+++ src/homeTab.js
@@ -1,16 +1,30 @@
+import Enjoyment from './images/enjoyment.jpg';
+
 export default function homeTab() {
   const content = document.getElementById('content');
   content.classList.add('card');
 
   // Move this to its own thing later. Needs to be reused in other tabs
-  const domElement = (tag, text, cssClass = false) => {
+  const textElement = (tag, text, cssClass = false) => {
     const element = document.createElement(tag);
     element.innerText = text;
     if (cssClass) { element.classList.add(cssClass) }
     return element;
   }
+  const addTextToContent = (tag, text, cssClass = false) => {
+    content.appendChild(textElement(tag, text, cssClass));
+  }
 
+  const addImageToContent = (name, cssClass = false) => {
+    let image = new Image();
+    image.src = Enjoyment;
+    if (cssClass) { image.classList.add(cssClass) }
+    content.appendChild(image);
+  }
 
-  content.appendChild(domElement('p', 'Added by function'))
+  addTextToContent('h1', 'Fully Loaded Nacho Emporium', 'restaurant_name')
+  addTextToContent('p', 'Come on down to the Fully Loaded Nacho Emporium. The only family-friendly, fully-loaded, nacho establishment in the greater metropolitan area that doesn\'t require you to be Fully Loaded')
+  addTextToContent('p', 'Added by function');
+  addImageToContent(Enjoyment);
 
 }
