5886335c97a5d1944d52baa0e5005124abfc314c
Quentin Pongratz <qpongratz@gmail.com>
2022-06-15 18:51:43 -0500

    Round the nachos

--- src/menuTab.js
+++ src/menuTab.js
@@ -1,5 +1,9 @@
-import {createElement, createList} from './elementCreator'
+import {createElement, createImage, createList} from './elementCreator'
+import Broiled from './images/broiled.jpeg';
+import Classic from './images/classic.jpg';
+import Deconstructed from './images/deconstructed.jpg';
+import RiceBowl from './images/rice-bowl.jpg';
 
 export default function homeTab(content) {
-
+  content.appendChild(createImage(Broiled));
 }
