182f417f89fb1228c5532cfac5f67f346428aff4
Quentin Pongratz <qpongratz@gmail.com>
2022-06-16 12:15:05 -0500

    Add contact content

--- src/contactTab.js
+++ src/contactTab.js
@@ -1,5 +1,11 @@
 import {createElement, createList} from './elementCreator'
 
 export default function contactTab(content) {
-
+  content.appendChild(createElement('h1', 'Get in Contact'));
+  const contactMethods = [
+    'Call: 405-555-1923',
+    'Email: RealNachoEmporiumEmail@example.com'
+  ]
+  content.appendChild(createList(contactMethods, false));
+  content.appendChild(createElement('p', 'Or just come visit us in person at the corner of street and avenue, downtown.'));
 }
